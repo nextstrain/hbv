83e5013c48daa3cade3d97aeb3f7a94e49ea2784
James Hadfield <hadfield.james@gmail.com>
2023-06-27 14:13:39 +1200

    Refactor genbank parse script
    Only minor changes to functionality, and more helpful summary statistics

--- ingest/scripts/parse-genbank.py
+++ ingest/scripts/parse-genbank.py
@@ -7,13 +7,15 @@ Most code was originally written by @kistlerk as part of `curate_genbank_to_fast
 
 import argparse
 from Bio import SeqIO
-from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
-from dateutil.parser import parse
-from collections import defaultdict, Counter
 import dateutil
+import dateutil.parser
+from collections import defaultdict
 import re
 import csv
+
+class MissingDate(Exception):
+    pass
 
 def parse_genotype(genbank_note):
     """
@@ -54,7 +56,7 @@ def parse_genotype(genbank_note):
                     genotype = 'None'
                     subgenotype = 'None'
                     
-    return genotype, subgenotype
+    return (genotype, subgenotype)
 
 def get_geno_from_match(match_result):
     if 'recombinant' in match_result or 'Recombinant' in match_result or '/' in match_result:
@@ -76,7 +78,11 @@ def get_geno_from_match(match_result):
         genotype = match_result.split('subgenotype ')[1][0].upper()
     else:
         genotype = match_result[0].upper()
-        if genotype not in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']:
+    
+    if genotype=="D4":
+        genotype = "D"
+
+    if genotype not in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'recombinant']:
             genotype = 'None'
 
     return genotype
@@ -105,7 +111,7 @@ def get_subgeno_from_match(genotype, match_result):
             if 1< len(potential_sub) <4:
                 subgenotype = potential_sub.upper()
                 if ')' in subgenotype:
-                    subgenotyppe = subgenotype.split(')')[0].upper()
+                    subgenotype = subgenotype.split(')')[0].upper()
             else:
                 subgenotype='None'
         elif ':' in match_result:
@@ -121,90 +127,65 @@ def get_subgeno_from_match(genotype, match_result):
 
     return subgenotype
 
-def main(input_fname, output_seqs_fname, output_meta_fname):
-    """
-    Takes a genbank file and
-    1. Removes synthetic sequences and clinical isolates
-    2. Extracts (interpets) metadata: collection_date, strain, country, host, genotype, subgenotype
-    3. Drops sequences without a collection_date
-    4. Writes out FASTA with header accession|strain_name|date|country|host|genotype|subgenotype
-    """
+def exclude_isolate(record):
+    #exclude patent and synthetic sequences, that are not clinical isolates
+    return record.annotations['data_file_division'] in ['PAT', 'SYN']
 
-    #store all edited sequence records
-    seq_records = []
+
+def summarise(metadata):
     counts = defaultdict(int)
+    n = 0
+    for data in metadata.values():
+        n+=1
+        for k,v in data.items():
+            if v != 'None':
+                counts[k]+=1
+    print("\nSummary of parsed metadata:")
+    for field, v in counts.items():
+        stars = "*" * int(round(v/n*20, 0))
+        print(f"{field:25}{stars:20} (n={v}/{n})")
+    print()
+
+def parse_collection_date(record, source):
+
+    collection_date = extract(source, 'collection_date')
+    if not collection_date:
+        collection_date = record.annotations['date']
+    
+    if collection_date == '2018/2019':
+        return False
+    
+    formatted_date = dateutil.parser.parse(collection_date).strftime('%Y-%m-%d')
+    #dateutil parser will assign a day (today's date) to unknown days, and same for month, want XX instead
+    if len(collection_date)==8:
+        formatted_date = formatted_date[:-2] + 'XX'
+    elif len(collection_date)==4:
+        # NOTE: around 1/4 sequences only have a year!
+        formatted_date = formatted_date[:5] + 'XX-XX'
+
+    return formatted_date
+
+def extract(feature, key):
+    if key in feature.qualifiers:
+         return feature.qualifiers[key][0]
+    return False
+
+def parse_metadata(record):
+    accession = record.id.split('.')[0]
+    source = [feature for feature in record.features if feature.type == 'source'][0]
+    note = extract(source, 'note')
+
     metadata = {}
-    for record in SeqIO.parse(open(input_fname,"r"), "genbank"):
-        counts['total']+=1
-        accession = record.id.split('.')[0]
-        collection_date, strain_name, country, host, genotype, subgenotype = 'None', 'None', 'None', 'None', 'None', 'None'
-        #exclude patent and synthetic sequences, that are not clinical isolates
-        if record.annotations['data_file_division'] not in ['PAT', 'SYN']:
-            
-            for feature in record.features:
-                if feature.type == 'source':
-                    if 'collection_date' in feature.qualifiers:
-                        collection_date = feature.qualifiers['collection_date'][0]   
-                    if 'strain' in feature.qualifiers:
-                        strain_name = feature.qualifiers['strain'][0]
-                    if 'country' in feature.qualifiers:
-                        country = feature.qualifiers['country'][0]
-                    if 'host' in feature.qualifiers:
-                        host = feature.qualifiers['host'][0]
-                    if 'note' in feature.qualifiers:
-                        if 'genotype' in feature.qualifiers['note'][0]:
-                            genotype, subgenotype = parse_genotype(feature.qualifiers['note'][0])
-            
-            if genotype == 'None':
-                counts['no_genotype']+=1
-            if subgenotype == 'None':
-                counts['no_subgenotype']+=1
-
-            if collection_date == 'None':
-                collection_date = record.annotations['date']
-
-            #only keep sequences with date
-            
-            if collection_date != 'None':
-                if collection_date != '2018/2019':
-                    formatted_date = dateutil.parser.parse(collection_date).strftime('%Y-%m-%d')
-                    #dateutil parser will assign a day (today's date) to unknown days, and same for month, want XX instead
-                    if len(collection_date)==8:
-                        formatted_date = formatted_date[:-2] + 'XX'
-                    elif len(collection_date)==4:
-                        formatted_date = formatted_date[:5] + 'XX-XX'
-                    
-                    
-                    counts['output']+=1
-                    metadata[accession] = {
-                        'name': accession, # TODO XXX - change once new augur release is out with https://github.com/nextstrain/augur/pull/1240,
-                        'accession': accession,
-                        'strain_name': strain_name,
-                        'collection_date': formatted_date,
-                        'country': country,
-                        'host': host,
-                        'genotype_genbank': genotype,
-                        'subgenotype_genbank': subgenotype
-                    }
-                    # list_of_info = [accession, strain_name, formatted_date, country, host, genotype, subgenotype]
-                    # new_record_info = '|'.join(list_of_info)
-                    seq_records.append(SeqRecord(record.seq, id=accession, description=''))  
-                else:
-                    counts['excluded_missing_date']+=1
-            else:
-                counts['excluded_missing_date']+=1
-
-    print(counts)
-
-    #write fasta sequence file for ALL sequences
-    SeqIO.write(seq_records, output_seqs_fname, "fasta")
-
-    with open(output_meta_fname, 'w') as fh:
-        writer = csv.writer(fh, delimiter='\t', quotechar='"')
-        header = list(next(iter(metadata.values())).keys())
-        writer.writerow(header)
-        for d in metadata.values():
-            writer.writerow([d[k] for k in header])
+    metadata['name'] = accession # TODO XXX - change once new augur release is out with https://github.com/nextstrain/augur/pull/1240, 
+    metadata['accession'] = accession
+    metadata['strain_name'] = extract(source, 'strain') or extract(source, 'isolate') or 'None'
+    metadata['country'] = extract(source, 'country') or 'None'
+    metadata['host'] = extract(source, 'host') or 'None'
+    metadata['genotype_genbank'], metadata['subgenotype_genbank'] = parse_genotype(note) if note else ("None", "None")
+    metadata['collection_date'] = parse_collection_date(record, source)
+    if not metadata['collection_date']:
+        raise MissingDate
+    return metadata
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -215,4 +196,35 @@ if __name__ == '__main__':
     parser.add_argument("--output-sequences")
     parser.add_argument("--output-metadata")
     args = parser.parse_args()
-    main(args.genbank, args.output_sequences, args.output_metadata)
+
+    n_records = 0
+    seq_records = []
+    exclude_counts = defaultdict(int)
+    metadata = {}
+
+    for record in SeqIO.parse(open(args.genbank,"r"), "genbank"):
+        n_records+=1
+
+        if exclude_isolate(record):
+            exclude_counts['patient/synthetic sequence']+=1
+            continue
+
+        try:
+            record_metadata = parse_metadata(record)
+        except MissingDate:
+            exclude_counts['missing date']+=1
+            continue   
+
+        seq_records.append(SeqRecord(record.seq, id=record_metadata['accession'], description=''))  
+        metadata[record_metadata['accession']] = record_metadata
+
+    print(f"{n_records} records parsed. Excluded "+', '.join([f"n={v} ({k})"for k,v in exclude_counts.items()]))
+    summarise(metadata)
+
+    SeqIO.write(seq_records, args.output_sequences, "fasta")
+    with open(args.output_metadata, 'w') as fh:
+        writer = csv.writer(fh, delimiter='\t', quotechar='"')
+        header = list(next(iter(metadata.values())).keys())
+        writer.writerow(header)
+        for d in metadata.values():
+            writer.writerow([d[k] for k in header])
